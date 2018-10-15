94921d6e29b372d37a8a3c79072a7a23f314d6fc
Muhammad Haekal <93haekal@gmail.com>
2018-10-15 14:59:02 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// console.log("JavaScript")
+
+// VARIABLE DECLARATION ------------------------------------------
+let num = 3
+let str1 = "string"
+let str2 = 'string'
+let str3 = `string`
+let bol = false
+let arr = [2,"string",true,[1,2,3]]
+let obj = {
+    name: "Gibrantem",
+    age: 200,
+    skills: ["C++","JavaScript","Markdown"],
+    married: false
+}
+
+
+// DISPLAY ARRAY ------------------------------------------
+// console.log("display all elements:",arr)
+// console.log("display single element:",arr[3])
+// console.log("two dimensional array :",arr[3][1])
+
+// DISPLAY OBJECT VALUE ------------------------------------------
+// console.log("display all object properties:",obj)
+// console.log("display one object property:",obj.skills)
+
+// ARRAY OF OBJECT ------------------------------------------
+
+let arr_of_obj = [
+    {
+        name: "Gibrantem",
+        age: 200,
+        skills: ["C++","JavaScript","Markdown"],
+        married: false
+    },
+    {
+        name: "Wippy",
+        age: 19,
+        skills: ["C++","Markup","Markdown"],
+        married: false
+    }
+]
+
+// DISPLAY DATA ------------------------------------------
+// console.log(arr_of_obj[0].skills[1])
+// console.log(arr_of_obj[1].name)
+// console.log(arr_of_obj[0].skills[1])
+// console.log(arr_of_obj[0].skills[1])
+
+
+
+
